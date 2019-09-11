3c0743eadb34ffffd066e64c79c4f30d20df0555
Philipp Gerhard <philipp.gerhard@me.com>
2019-09-11 22:41:35 +0200

    Made changes as described in: https://github.com/electron-userland/electron-forge/issues/1113#issue-485618895 https://github.com/electron-userland/electron-forge/issues/1113#issuecomment-525205828

--- typescript-sample/webpack.main.config.js
+++ typescript-sample/webpack.main.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  /**
-   * This is the main entry point for your application, it's the first file
-   * that runs in the main process.
-   */
-  entry: './src/main.js',
-  // Put your normal webpack config below here
-  module: {
-    rules: require('./webpack.rules'),
+  // Changes based on https://github.com/TypeStrong/ts-loader#configuration
+  mode: "development",
+  devtool: "inline-source-map",
+  // From Template (Start)
+  entry: "./src/main.ts",
+  // From Template (End)
+  // Changes based on https://github.com/TypeStrong/ts-loader#configuration
+  output: {
+    filename: "./.webpack/main.ts"
   },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  // From Template (Start): Put your normal webpack config below here
+  module: {
+    rules: require("./webpack.rules")
+  },
+  // From Template (End)
 };
